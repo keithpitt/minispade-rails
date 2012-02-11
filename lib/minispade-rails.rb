bb63b81a4418f174c18c36eca5a3ef7fd6f71ea5
Keith Pitt <me@keithpitt.com>
2012-02-11 15:02:31 +1100

    Initial commit

---
+++ lib/minispade-rails.rb
@@ -0,0 +1,7 @@
+require "minispade-rails/version"
+
+module Minispade
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/minispade-rails/version.rb
@@ -0,0 +1,5 @@
+module Minispade
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
