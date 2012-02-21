79ea58c397d27197054ec6415f20fd6db0e957b9
Keith Pitt <me@keithpitt.com>
2012-02-22 10:05:51 +1100

    Added a way to turn off delayed parsing

--- lib/minispade-rails/compiler.rb
+++ lib/minispade-rails/compiler.rb
@@ -5,6 +5,14 @@ module MinispadeRails
   # = Sprockets engine for HandlebarsJS templates
   class Compiler < Tilt::Template
 
+    def self.deferred=(val)
+      @deferred = val
+    end
+
+    def self.deferred
+      @deferred
+    end
+
     def self.default_mime_type
       'application/javascript'
     end
@@ -16,7 +24,10 @@ module MinispadeRails
     end
 
     def evaluate(scope, locals, &block)
-      "minispade.register(\"#{scope.logical_path}\", #{data.inspect});\n"
+      if MinispadeRails::Compiler.deferred
+        "minispade.register(\"#{scope.logical_path}\", #{data.inspect});\n"
+      else
+        "minispade.register(\"#{scope.logical_path}\", function() { #{data} });\n"
     end
 
   end
